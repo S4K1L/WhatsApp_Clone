e43f37e5ba44354a850c043fab8b158a4210c6fe
Shakil Mahmud <69shakilmahmud@gmail.com>
2024-09-14 18:11:59 +0600

    Lv0.14.09.24 DONE without OTP

--- lib/features/select_contacts/screens/select_contact_screen.dart
+++ lib/features/select_contacts/screens/select_contact_screen.dart
@@ -25,6 +25,7 @@ class SelectContactsScreen extends ConsumerWidget {
     );
   }
 
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
@@ -45,55 +46,64 @@ class SelectContactsScreen extends ConsumerWidget {
           ),
         ],
       ),
-      body: ref.watch(getUsersProvider).when(
-        data: (userList) => ListView.builder(
-          itemCount: userList.length,
-          itemBuilder: (context, index) {
-            final user = userList[index];
-            return Padding(
-              padding: const EdgeInsets.only(bottom: 10,top: 16,left: 10,right: 10),
-              child: Container(
-                height: 80,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    color: Colors.grey[700]!.withOpacity(0.3)
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    InkWell(
-                      onTap: () => selectContact(context, user),
-                      child: ListTile(
-                        title: Text(
-                          user.name,
-                          style: const TextStyle(
-                            fontSize: 18,
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/backgroundImage.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: ref.watch(getUsersProvider).when(
+          data: (userList) => ListView.builder(
+            itemCount: userList.length,
+            itemBuilder: (context, index) {
+              final user = userList[index];
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 10,top: 16,left: 10,right: 10),
+                child: Container(
+                  height: 80,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      color: Colors.grey[700]!.withOpacity(0.3)
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      InkWell(
+                        onTap: () => selectContact(context, user),
+                        child: ListTile(
+                          title: Text(
+                            user.name,
+                            style: const TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                          subtitle: Text(
+                            '0${user.email.length > 10 ? user.email.substring(0, 10) : user.email}',
+                          ),
+                          leading: user.profilePic.isEmpty
+                              ? CircleAvatar(
+                            backgroundColor: Colors.grey[200],
+                            child: const Icon(Icons.person),
+                            radius: 30,
+                          )
+                              : CircleAvatar(
+                            backgroundImage: NetworkImage(user.profilePic),
+                            radius: 30,
                           ),
                         ),
-                        subtitle: Text(
-                          '0${user.email.length > 10 ? user.email.substring(0, 10) : user.email}',
-                        ),
-                        leading: user.profilePic.isEmpty
-                            ? CircleAvatar(
-                          backgroundColor: Colors.grey[200],
-                          child: const Icon(Icons.person),
-                          radius: 30,
-                        )
-                            : CircleAvatar(
-                          backgroundImage: NetworkImage(user.profilePic),
-                          radius: 30,
-                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            );
-          },
+              );
+            },
+          ),
+          error: (err, trace) => ErrorScreen(error: err.toString()),
+          loading: () => const Loader(),
         ),
-        error: (err, trace) => ErrorScreen(error: err.toString()),
-        loading: () => const Loader(),
       ),
     );
   }
